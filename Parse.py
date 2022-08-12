16c8e50ef09fbd23f75eaeaabe1a93975cb2749c
Profesoor <ariel3333876@gmail.com>
2022-08-12 18:41:54 +0300

    parsing now works good. you can check for any kind of example.txt you want

--- Parse.py
+++ Parse.py
@@ -1,8 +1,7 @@
-import util
 import numpy as np
 from scipy.io import wavfile
 
-BPM = 100
+BPM = 120
 DICT = {'$':0, 'A':37, 'A#':38, 'Bb':38, 'B':39, 'C':40, 'C#':41, 'Db':41, 'D':42, 'D#':43, 'Eb':43, 'E':44, 'F':45, 'F#':46, 'Gb':46, 'G':47, 'G#':48, 'Ab':48} #create dictionary for each note and placement
 BEAT_DURATION = 60 / BPM #the duration of each beat in s, by the bpm
 SAMPLE_RATE = 44100
@@ -20,7 +19,26 @@ def parse_notes(path):
             if beat in DICT:
                 song.append(get_wave(calculate_freq(beat), BEAT_DURATION))
             else:
-                print("multiple notes")
+                a = []
+                if len(beat) == 2:
+                    a = list(beat)
+                else:
+                    for i in range(len(beat)-1):
+                        if beat[i+1] == "b" or beat[i+1] == "#":
+                            a.append(beat[i] + beat[i+1])
+                            if i == len(beat)-1:
+                                a.append(beat[-1])
+                            i+=2
+                        elif beat[i] in DICT:
+                            a.append(beat[i])
+                            if i == len(beat)-2:
+                                a.append(beat[-1])
+                        elif beat[i+1] in DICT:
+                            a.append(beat[i+1])
+                        else:
+                            pass
+                for n in a:
+                    song.append(get_wave(calculate_freq(n), BEAT_DURATION/len(a)))
     f.close()
     write_notes(song)
 
